8079993564094b3db8041b769207e8a50f2ee789
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-10-27 16:17:10 -0700

    run tests on Stylist class while building methods. Begin same process on Client class.

--- HairSalon.Solution/HairSalon.Tests/Models.Tests/Stylist.Tests.cs
+++ HairSalon.Solution/HairSalon.Tests/Models.Tests/Stylist.Tests.cs
@@ -26,5 +26,71 @@ namespace HairSalon.Models.Tests
 
       Assert.AreEqual(0, result);
     }
+
+    [TestMethod]
+    public void Equals_ReturnTrueForSameName_Stylist()
+    {
+      Stylist stylist1 = new Stylist("Michelle", 50, "Cut and Color", 1);
+      Stylist stylist2 = new Stylist("Michelle", 50, "Cut and Color", 1);
+
+      Assert.AreEqual(stylist1, stylist2);
+    }
+
+    [TestMethod]
+    public void Save_SavesStylistToDatabase_Stylist()
+    {
+      Stylist testStylist = new Stylist("Michelle", 50, "Cut and Color", 1);
+      testStylist.Save();
+
+      List<Stylist> result = Stylist.GetAll();
+      List<Stylist> testList = new List<Stylist>{testStylist};
+
+      CollectionAssert.AreEqual(testList, result);
+    }
+
+    [TestMethod]
+    public void Find_FindStylistInDatabase_Stylist()
+    {
+      Stylist testStylist = new Stylist("Michelle", 50, "Cut and Color", 1);
+      testStylist.Save();
+
+      Stylist foundStylist = Stylist.Find(testStylist.Id);
+
+      Assert.AreEqual(testStylist, foundStylist);
+    }
+
+    [TestMethod]
+    public void Update_UpdateStylistInDatabase_Stylist()
+    {
+      Stylist testStylist = new Stylist("Michelle", 50, "Cut and Color", 1);
+      testStylist.Save();
+
+      string newName = "Claire";
+      int newRate = 55;
+      string newSkills = "Lash Extensions, Cut and Color";
+
+      Stylist newStylist = new Stylist(newName, newRate, newSkills, 1);
+      testStylist.Update(newName, newRate, newSkills);
+
+      Assert.AreEqual(newStylist.Name, testStylist.Name);
+      Assert.AreEqual(newStylist.Rate, testStylist.Rate);
+      Assert.AreEqual(newStylist.Skills, testStylist.Skills);
+    }
+
+    [TestMethod]
+    public void Delete_DeleteStylistInDatabase_Stylist()
+    {
+      Stylist stylist1 = new Stylist("Michelle", 50, "Cut and Color", 1);
+      stylist1.Save();
+      Stylist stylist2 = new Stylist("Claire", 55, "Lash Extensions, Cut and Color", 2);
+      stylist2.Save();
+
+      List<Stylist> testList = new List<Stylist>{stylist2};
+      stylist1.Delete();
+
+      List<Stylist> result = Stylist.GetAll();
+
+      CollectionAssert.AreEqual(testList, result);
+    }
   }
 }
--- HairSalon.Solution/HairSalon/Models/Stylist.cs
+++ HairSalon.Solution/HairSalon/Models/Stylist.cs
@@ -61,5 +61,159 @@ namespace HairSalon.Models
       }
       return allStylists;
     }
+
+    public override bool Equals(System.Object otherStylist)
+    {
+      if (!(otherStylist is Stylist))
+      {
+        return false;
+      }
+      else
+      {
+        Stylist newStylist = (Stylist) otherStylist;
+        bool idEquality = (this.Id == newStylist.Id);
+        bool nameEquality = (this.Name == newStylist.Name);
+        bool rateEquality = (this.Rate == newStylist.Rate);
+        bool skillsEquality = (this.Skills == newStylist.Skills);
+        return (idEquality && nameEquality && rateEquality && skillsEquality);
+      }
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO stylists (name, rate, skills) VALUES (@StylistName, @StylistRate, @StylistSkills);";
+
+      //name
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@StylistName";
+      name.Value = this.Name;
+      cmd.Parameters.Add(name);
+
+      //rate
+      MySqlParameter rate = new MySqlParameter();
+      rate.ParameterName = "@StylistRate";
+      rate.Value = this.Rate;
+      cmd.Parameters.Add(rate);
+
+      //skills
+      MySqlParameter skills = new MySqlParameter();
+      skills.ParameterName = "@StylistSkills";
+      skills.Value = this.Skills;
+      cmd.Parameters.Add(skills);
+
+      cmd.ExecuteNonQuery();
+      Id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static Stylist Find(int searchId)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM stylists WHERE id = @thisId;";
+
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = searchId;
+      cmd.Parameters.Add(thisId);
+
+      int stylistId = 0;
+      string stylistName = "";
+      int stylistRate = 0;
+      string stylistSkills = "";
+
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        stylistId = rdr.GetInt32(0);
+        stylistName = rdr.GetString(1);
+        stylistRate = rdr.GetInt32(2);
+        stylistSkills = rdr.GetString(3);
+      }
+
+      Stylist foundStylist = new Stylist(stylistName, stylistRate, stylistSkills, stylistId);
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundStylist;
+    }
+
+    public void Update(string newName, int newRate, string newSkills)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE stylists SET name = @newName, rate = @newRate, skills = @newSkills WHERE id = @searchId;";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = Id;
+      cmd.Parameters.Add(searchId);
+
+      //name
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@newName";
+      name.Value = newName;
+      cmd.Parameters.Add(name);
+
+      //rate
+      MySqlParameter rate = new MySqlParameter();
+      rate.ParameterName = "@newRate";
+      rate.Value = newRate;
+      cmd.Parameters.Add(rate);
+
+      //skills
+      MySqlParameter skills = new MySqlParameter();
+      skills.ParameterName = "@newSkills";
+      skills.Value = newSkills;
+      cmd.Parameters.Add(skills);
+
+      cmd.ExecuteNonQuery();
+      Name = newName;
+      Rate = newRate;
+      Skills = newSkills;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public void Delete()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM stylists where id = @searchId;";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = this.Id;
+      cmd.Parameters.Add(searchId);
+
+      cmd.ExecuteNonQuery();
+      conn.Close();
+
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
   }
 }
