e2c5087ca5d1d00c9db2d84ffbe5f0e371278dc9
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-10-27 18:06:45 -0700

    run tests on Client class while building methods, begin home controller set up and cshtml setup.

--- HairSalon.Solution/HairSalon.Tests/Models.Tests/Client.Tests.cs
+++ HairSalon.Solution/HairSalon.Tests/Models.Tests/Client.Tests.cs
@@ -1,23 +1,111 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Text.RegularExpressions;
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using HairSalon.Models;
-//
-// namespace HairSalon.Models.Tests
-// {
-//   [TestClass]
-//   public class ClientTests : IDisposable
-//   {
-//     public ClientTests()
-//     {
-//     DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=emily_jimenez_test;";
-//     }
-//
-//     public void Dispose()
-//     {
-//       Client.ClearAll();
-//     }
-//
-//   }
-// }
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon.Models;
+
+namespace HairSalon.Models.Tests
+{
+  [TestClass]
+  public class ClientTests : IDisposable
+  {
+    public ClientTests()
+    {
+    DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=emily_jimenez_test;";
+    }
+
+    public void Dispose()
+    {
+      Client.ClearAll();
+    }
+
+    [TestMethod]
+    public void GetAll_ClientEmptyAtFirst_0()
+    {
+      int result = Client.GetAll().Count;
+      Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void GetAllClientsByStylist_GetsClientsInDatabaseByStylistId_ClientList()
+    {
+      Client client1 = new Client("Joanne", 092982, "joanneS@me.com", 1);
+      client1.Save();
+      Client client2 = new Client("Cynthia", 101188, "c.smith@.com", 2);
+      client2.Save();
+      Client client3 = new Client("Becky", 121479, "b_sanchez@yahoo.com", 1);
+      client3.Save();
+
+      List<Client> testList = Client.GetAllClientsByStylist(1);
+      List<Client> expectedList = new List<Client>{client1, client3};
+
+      CollectionAssert.AreEqual(testList, expectedList);
+    }
+
+    [TestMethod]
+    public void Equals_ReturnsTrueIfClientsAreTheSame_Client()
+    {
+      Client client1 = new Client("Joanne", 092982, "joanneS@me.com", 1);
+      Client client2 = new Client("Joanne", 092982, "joanneS@me.com", 1);
+
+      Assert.AreEqual(client1, client2);
+    }
+
+    [TestMethod]
+    public void Save_SavesClientToDatabase_ClientList()
+    {
+      Client testClient = new Client("Joanne", 092982, "joanneS@me.com", 1);
+      testClient.Save();
+
+      List<Client> result = Client.GetAll();
+      List<Client> testList = new List<Client>{testClient};
+
+      CollectionAssert.AreEqual(testList, result);
+    }
+
+    [TestMethod]
+    public void Find_FindsClientInDatabase_Client()
+    {
+      Client testClient = new Client("Joanne", 092982, "joanneS@me.com", 1);
+      testClient.Save();
+
+      Client foundClient = Client.Find(testClient.Id);
+
+      Assert.AreEqual(testClient, foundClient);
+    }
+
+    [TestMethod]
+    public void Update_UpdateClientInDatabase_Client()
+    {
+      Client testClient = new Client("Joanne", 092982, "joanneS@me.com", 1);
+      testClient.Save();
+
+      string newName = "Cynthia";
+      int newBirthday = 101188;
+      string newEmail = "c.smith@gmail.com";
+
+      Client newClient = new Client(newName, newBirthday, newEmail, 1);
+      testClient.Update(newName, newBirthday, newEmail);
+
+      Assert.AreEqual(newClient.Name, testClient.Name);
+      Assert.AreEqual(newClient.Birthday, testClient.Birthday);
+      Assert.AreEqual(newClient.Email, testClient.Email);
+    }
+
+    [TestMethod]
+    public void Delete_DeletesClientInDatabase_Client()
+    {
+      Client client1 = new Client("Joanne", 092982, "joanneS@me.com", 1);
+      client1.Save();
+      Client client2 = new Client("Cynthia", 101188, "c.smith@.com", 2);
+      client2.Save();
+
+      List<Client> testList = new List<Client>{client2};
+      client1.Delete();
+
+      List<Client> result = Client.GetAll();
+
+      CollectionAssert.AreEqual(testList, result);
+    }
+  }
+}
--- HairSalon.Solution/HairSalon/Models/Client.cs
+++ HairSalon.Solution/HairSalon/Models/Client.cs
@@ -38,5 +38,224 @@ namespace HairSalon.Models
           conn.Dispose();
         }
       }
+
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientName = rdr.GetString(1);
+        int clientBirthday = rdr.GetInt32(2);
+        string clientEmail = rdr.GetString(3);
+        int stylistId = rdr.GetInt32(4);
+
+        Client newClient = new Client(clientName, clientBirthday, clientEmail, stylistId, clientId);
+        allClients.Add(newClient);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allClients;
+    }
+
+    public static List<Client> GetAllClientsByStylist(int inputId)
+    {
+      List<Client> stylistClients = new List<Client>{};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients WHERE stylist_id = @stylistId;";
+
+      MySqlParameter stylistId = new MySqlParameter();
+      stylistId.ParameterName = "@stylistId";
+      stylistId.Value = inputId;
+      cmd.Parameters.Add(stylistId);
+
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int returnId = rdr.GetInt32(0);
+        string returnName = rdr.GetString(1);
+        int returnBirthday = rdr.GetInt32(2);
+        string returnEmail = rdr.GetString(3);
+        int returnStylistId = rdr.GetInt32(4);
+        Client returnClient = new Client(returnName, returnBirthday, returnEmail, returnStylistId, returnId);
+        stylistClients.Add(returnClient);
+      }
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return stylistClients;
+    }
+
+    public override bool Equals(System.Object otherClient)
+    {
+      if (!(otherClient is Client))
+      {
+        return false;
+      }
+      else
+      {
+        Client newClient = (Client) otherClient;
+        bool idEquality = (this.Id == newClient.Id);
+        bool nameEquality = (this.Name == newClient.Name);
+        bool birthdayEquality = (this.Birthday == newClient.Birthday);
+        bool emailEquality = (this.Email == newClient.Email);
+        bool stylistIdEquality = (this.StylistId == newClient.StylistId);
+        return (idEquality && nameEquality && birthdayEquality && emailEquality && stylistIdEquality);
+      }
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO clients (name, birthday, email, stylist_id) VALUES (@ClientName, @ClientBirthday, @ClientEmail, @ClientStylistId);";
+      //name
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@ClientName";
+      name.Value = this.Name;
+      cmd.Parameters.Add(name);
+      //birthday
+      MySqlParameter birthday = new MySqlParameter();
+      birthday.ParameterName = "@ClientBirthday";
+      birthday.Value = this.Birthday;
+      cmd.Parameters.Add(birthday);
+      //email
+      MySqlParameter email = new MySqlParameter();
+      email.ParameterName = "@ClientEmail";
+      email.Value = this.Email;
+      cmd.Parameters.Add(email);
+      //stylist
+      MySqlParameter stylistId = new MySqlParameter();
+      stylistId.ParameterName = "@ClientStylistId";
+      stylistId.Value = this.StylistId;
+      cmd.Parameters.Add(stylistId);
+
+      cmd.ExecuteNonQuery();
+      Id = (int)cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static Client Find(int searchId)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients WHERE id = @thisId;";
+
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = searchId;
+
+      cmd.Parameters.Add(thisId);
+
+      int clientId = 0;
+      string clientName = "";
+      int clientBirthday = 0;
+      string clientEmail = "";
+      int clientStylistId = 0;
+
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        clientId = rdr.GetInt32(0);
+        clientName = rdr.GetString(1);
+        clientBirthday = rdr.GetInt32(2);
+        clientEmail = rdr.GetString(3);
+        clientStylistId = rdr.GetInt32(4);
+      }
+
+      Client foundClient = new Client(clientName, clientBirthday, clientEmail, clientStylistId, clientId);
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundClient;
+    }
+
+    public void Update(string newName, int newBirthday, string newEmail)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE clients SET name = @newName, birthday = @newBirthday, email = @newEmail WHERE id = @searchId;";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = Id;
+      cmd.Parameters.Add(searchId);
+
+      //name
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@newName";
+      name.Value = newName;
+      cmd.Parameters.Add(name);
+      //birthday
+      MySqlParameter birthday = new MySqlParameter();
+      birthday.ParameterName = "@newBirthday";
+      birthday.Value = newBirthday;
+      cmd.Parameters.Add(birthday);
+      //email
+      MySqlParameter email = new MySqlParameter();
+      email.ParameterName = "@newEmail";
+      email.Value = newEmail;
+      cmd.Parameters.Add(email);
+
+      cmd.ExecuteNonQuery();
+      Name = newName;
+      Birthday = newBirthday;
+      Email = newEmail;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public void Delete()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM clients WHERE id = @searchId;";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = this.Id;
+      cmd.Parameters.Add(searchId);
+
+      cmd.ExecuteNonQuery();
+      conn.Close();
+
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
   }
 }
